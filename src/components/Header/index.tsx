4cc6367986e76eeaf67641452e0c5685f1b0c8e3
Julia Pugliese <julia03.pugliese@gmail.com>
2024-03-22 11:50:27 -0300

    Alteração Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+import "./styles.css"
+import NavItems from "../NavItems";
+import DropDown from "../DropDown";
+
+
+export function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <NavItems link="https://www.salesforce.com/br" id="logo" img={<img src="salesforce-logo.png" alt="Logo da SalesForce"/>}/>
+
+                    <DropDown text="Produtos"/>
+
+                    <NavItems link="http://salesforce.com" text="Empresa"/>
+                    <NavItems link="http://salesforce.com" text="Suporte"/>
+                    <NavItems link="http://salesforce.com" text="Aprendizado"/>
+
+                </ul>
+    
+                <ul>
+     
+                    <NavItems id="icone-busca" link="http://salesforce.com">
+                        <img className="logo-barra-pesquisa"  src="search-icon2.svg" alt=""/>
+                    </NavItems>
+
+                    <NavItems id="login" link="http://salesforce.com" text="Login"/>
+                    <NavItems id="teste-gratis" link="http://salesforce.com" text="Teste Grátis"/>
+                </ul>
+            </nav>
+        </header>
+    );
+}
