28c2a5350c48efeb81bc064ba4176c1f9cde4e1f
Gabriel <rm553635@fiap.com.br>
2024-04-01 18:24:00 -0300

    Alterações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,28 @@
 'use client'
 import "./styles.css"
 import NavItems from "../NavItems";
-import DropDown from "../DropDown";
-
+import React, { useState } from 'react';
 
 export function Header() {
+    const [isNavActive, setIsNavActive] = useState(false);
+
     return (
         <header>
-            <nav>
-                <NavItems link="https://www.salesforce.com/br" id="logo" img={<img src="salesforce-logo.png" alt="Logo da SalesForce"/>}/>
-                <button className="hamburguer"></button>
-                <ul className="ul1">
+            <nav className={`nav ${isNavActive ? 'active' : ''}`}>
+                <div>
+                    <NavItems link="https://www.salesforce.com/br" id="logo" img={<img src="salesforce-logo.png" alt="Logo da SalesForce"/>}/>
+                    <button className="hamburguer" onClick={() => setIsNavActive(!isNavActive)}></button>
 
-                    <NavItems link="http://salesforce.com" text="Produtos"/>
-                    <NavItems link="http://salesforce.com" text="Empresa"/>
-                    <NavItems link="http://salesforce.com" text="Suporte"/>
-                    <NavItems link="http://salesforce.com" text="Aprendizado"/>
+                    <ul className="ul1">
+                        <NavItems link="http://salesforce.com" text="Produtos"/>
+                        <NavItems link="http://salesforce.com" text="Empresa"/>
+                        <NavItems link="http://salesforce.com" text="Suporte"/>
+                        <NavItems link="http://salesforce.com" text="Aprendizado"/>
+                    </ul>
+                </div>
 
-                </ul>
-    
                 <ul className="ul2">
-     
-                    <NavItems id="icone-busca" link="http://salesforce.com" img={<img className="logo-barra-pesquisa"  src="search-icon2.svg" alt=""/>}/>
-
+                    <NavItems id="icone-busca" link="http://salesforce.com" img={<img className="logo-barra-pesquisa" src="search-icon2.svg" alt=""/>}/>
                     <NavItems id="login" link="produtos" text="Login"/>
                     <NavItems id="teste-gratis" link="http://salesforce.com" text="Teste Grátis"/>
                 </ul>
