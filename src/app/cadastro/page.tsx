66f92a284b59a370ea177694c9532710a25382a1
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-20 10:17:54 -0300

    finalização da página de cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import "./styles.css";
 import NavItems from "@/components/NavItems";
 import React, { useState } from 'react';
 import Button from "@/components/Button";
@@ -68,99 +69,91 @@ export default function Cadastro() {
 
    
     return (
-        <div>
+        <div className="pagina-cadastro">
             <div>
                 <img src="salesforce-logo.png" alt="Logo da SalesForce"/>    
                 <h2>Experimente nossa solução completa de CRM e vendas, grátis por 30 dias.</h2>
-                <p>Com o Sales Cloud Professional Edition, você tem acesso a:</p>
-                <ul>
-                    <li>Dados pré-carregados e recursos para integrar os dados da sua empresa;</li>
-                    <li>Processos, relatórios e dashboards pré-configurados;</li>
-                    <li>Experiências guiadas para representantes, líderes e gestores de vendas;</li>
-                    <li>Guias e outros materiais sobre boas práticas de vendas;</li>
-                    <li>Onboarding integrado, treinamentos e webinars online;</li>
-                    <li>Configuração de pontuação e roteamento de leads;</li>
-                    <li>Ferramentas para automação de tarefas recorrentes;</li>
-                    <li>Uma visão completa da performance de seus representantes e equipes de vendas.</li>
-                </ul>
                 <div>
-                    <NavItems id="saiba-mais" link="http://salesforce.com" text="Saiba Mais"/>
-                    <NavItems id="teste-gratis-section" link="http://salesforce.com" text="Teste Grátis"/>     
-                </div>   
+                    <p>Com o Sales Cloud Professional Edition, você tem acesso a:</p>
+                    <ul>
+                        <li>Dados pré-carregados e recursos para integrar os dados da sua empresa;</li>
+                        <li>Processos, relatórios e dashboards pré-configurados;</li>
+                        <li>Experiências guiadas para representantes, líderes e gestores de vendas;</li>
+                        <li>Guias e outros materiais sobre boas práticas de vendas;</li>
+                        <li>Onboarding integrado, treinamentos e webinars online;</li>
+                        <li>Configuração de pontuação e roteamento de leads;</li>
+                        <li>Ferramentas para automação de tarefas recorrentes;</li>
+                        <li>Uma visão completa da performance de seus representantes e equipes de vendas.</li>
+                    </ul>
+                    <div className="botoes">
+                        <NavItems id="saiba-mais" link="http://salesforce.com" text="Saiba Mais"/>
+                        <NavItems id="teste-gratis-section" link="http://salesforce.com" text="Teste Grátis"/>     
+                    </div>   
+                </div>
+                
             </div>
-            <div>
-                <h3>Nome de Usuário</h3>
+            <div className="form">
+                <h3>Preencha com os seus dados</h3>
                 <FormInput className="input-field" placeholder="Nome de Usuário" value={form.username} onChange={
                     (event: any) => {
                     changeState('username', event.target.value);
                 } }/>
 
-                <h3>Senha</h3>
                 <FormInput className="input-field" placeholder="Senha" value={form.password} onChange={
                     (event: any) => {
                     changeState('password', event.target.value);
                 } }/>
 
 
-                <h3>Nome Completo</h3>
                 <FormInput className="input-field" placeholder="Nome Completo" value={form.nome} onChange={
                     (event: any) => {
                     changeState('nome', event.target.value);
                 } }/>
 
 
-                <h3>CPF</h3>
                 <FormInput className="input-field" placeholder="CPF" value={form.cpf} onChange={
                     (event: any) => {
                     changeState('cpf', event.target.value);
                 } }/>
                 
 
-                <h3>Cargo</h3>
                 <FormInput className="input-field" placeholder="Cargo" value={form.cargo} onChange={
                     (event: any) => {
                     changeState('cargo', event.target.value);
                 } }/>
 
-                <h3>Email Corporativo</h3>
                 <FormInput className="input-field" placeholder="Email Corporativo" value={form.email} onChange={
                     (event: any) => {
                     changeState('email', event.target.value);
                 } }/>
 
-                <h3>Telefone</h3>
                 <FormInput className="input-field" placeholder="Telefone" value={form.telefone} onChange={
                     (event: any) => {
                     changeState('telefone', event.target.value);
                 } }/>
 
-                <h3>Empresa</h3>
                 <FormInput className="input-field" placeholder="Nome da empresa" value={form.empresa} onChange={
                     (event: any) => {
                     changeState('empresa', event.target.value);
                 } }/>
 
                 
-                <h3>CNPJ</h3>
                 <FormInput className="input-field" placeholder="CNPJ" value={form.cnpj} onChange={
                     (event: any) => {
                     changeState('cnpj', event.target.value);
                 } }/>
 
                 
-                <h3>Segmento</h3>
                 <FormInput className="input-field" placeholder="Segmento" value={form.segmento} onChange={
                     (event: any) => {
                     changeState('segmento', event.target.value);
                 } }/>
 
-                <h3>Tamanho da Empresa</h3>
                 <FormInput className="input-field" placeholder="Tamanho da Empresa" value={form.tamanhoEmpresa} onChange={
                     (event: any) => {
                     changeState('tamanhoEmpresa', event.target.value);
                 } }/>
 
-                <h3>País</h3>
                 <FormInput className="input-field" placeholder="País" value={form.pais} onChange={
                     (event: any) => {
                     changeState('pais', event.target.value);
